f93e039268061d800905a3fd39e53d5ad533595e
Cong Le <longchike@gmail.com>
2024-01-31 22:21:22 -0500

    initial set up

---
+++ Objc in Swift app/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Objc in Swift app
+//
+//  Created by Cong Le on 1/31/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        super.view.backgroundColor = .yellow
+    }
+}
+
